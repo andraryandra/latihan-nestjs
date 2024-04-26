08d0d345582506b39a88c61e2a5a6ea6dac50c18
Aryand <andraryandra22@student.polindra.ac.id>
2024-04-26 16:33:25 +0700

    feat: add feature login/register/profile

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,23 +1,31 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, Controller, Get, Post, Req, UseGuards } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { RegisterDto } from './dto/register.dto';
 import { LoginDto } from './dto/login.dto';
+import { AuthGuard } from './auth.guard';
 
 @Controller('auth')
 export class AuthController {
+  constructor(private authService: AuthService) {}
 
-    constructor(private authService: AuthService) {}
+  // /auth/register
+  @Post('/register')
+  async register(@Body() data: RegisterDto) {
+    return await this.authService.register(data);
+  }
 
-    // /auth/register
-    @Post('/register')
-    async register(@Body() data: RegisterDto) {
-        return await this.authService.register(data);
-    }
+  // /auth/login
+  @Post('/login')
+  async login(@Body() data: LoginDto) {
+    return await this.authService.login(data);
+  }
 
-    // /auth/login
-    @Post('/login')
-    async login(@Body() data: LoginDto) {
-        return await this.authService.login(data);
-    }
+  @UseGuards(AuthGuard)
+  @Get('/profile')
+  async profile(@Req() req) {
+    const userId = req.user.id;
+    const profile = await this.authService.profile(userId);
 
+    return profile;
+}
 }
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -3,67 +3,108 @@ import { PrismaService } from 'src/prisma/prisma.service';
 import { RegisterDto } from './dto/register.dto';
 import { compare, hash } from 'bcrypt';
 import { LoginDto } from './dto/login.dto';
+import { JwtService } from '@nestjs/jwt';
+import { jwt_config } from 'src/config/config_jwt';
 
 @Injectable()
 export class AuthService {
-    constructor(private prisma: PrismaService) {}
+  constructor(
+    private prisma: PrismaService,
+    private jwtService: JwtService,
+  ) {}
 
-    /**
-     * Register
-     * @param data 
-     * @returns 
-     */
-    async register(data: RegisterDto) {
-        const checkUser = await this.prisma.users.findFirst({
-            where: {
-                email: data.email
-            }
-        });
+  /**
+   * Register
+   * @param data
+   * @returns
+   */
+  async register(data: RegisterDto) {
+    const checkUser = await this.prisma.users.findFirst({
+      where: {
+        email: data.email,
+      },
+    });
 
-        if (checkUser) {
-            throw new HttpException('User already exists', HttpStatus.FOUND);
-        }
-
-        data.password = await hash(data.password, 12);
-        const createUser = await this.prisma.users.create({
-            data,
-        });
-
-        if (createUser) {
-            return {
-                statusCode: HttpStatus.OK,
-                message: 'User has been created',
-                data: createUser,
-            }
-        }
+    if (checkUser) {
+      throw new HttpException('User already exists', HttpStatus.FOUND);
     }
 
-    /**
-     * Login User
-     * @param data
-     * @returns
-     */
-    async login(data: LoginDto) {
-        const checkUser = await this.prisma.users.findFirst({
-            where: {
-                email: data.email
-            }
-        });
+    data.password = await hash(data.password, 12);
+    const createUser = await this.prisma.users.create({
+      data,
+    });
 
-        if (!checkUser) {
-            throw new HttpException('User not found', HttpStatus.NOT_FOUND);
-        }
-
-        const checkPassword = await compare(data.password, checkUser.password);
-
-        if (checkPassword) {
-            return {
-                statusCode: HttpStatus.OK,
-                message: 'Login success',
-                data: checkUser,
-            }
-        } else {
-            throw new HttpException('Password not match', HttpStatus.UNAUTHORIZED);
-        }
+    if (createUser) {
+      return {
+        statusCode: HttpStatus.OK,
+        message: 'User has been created',
+        data: createUser,
+      };
     }
+  }
+
+  /**
+   * Login User
+   * @param data
+   * @returns
+   */
+  async login(data: LoginDto) {
+    const checkUser = await this.prisma.users.findFirst({
+      where: {
+        email: data.email,
+      },
+    });
+
+    if (!checkUser) {
+      throw new HttpException('User not found', HttpStatus.NOT_FOUND);
+    }
+
+    const checkPassword = await compare(data.password, checkUser.password);
+
+    if (checkPassword) {
+      const accessToken = this.generateJWT({
+        sub: checkUser.id,
+        email: checkUser.email,
+        name: checkUser.name,
+      });
+      return {
+        statusCode: HttpStatus.OK,
+        message: 'Login success',
+        data: checkUser,
+        accessToken: accessToken,
+      };
+    } else {
+      throw new HttpException('Password not match', HttpStatus.UNAUTHORIZED);
+    }
+  }
+
+  async profile(user_id: number) {
+    const user = await this.prisma.users.findUnique({
+        where: {
+            id: user_id
+        },
+        select: {
+            id: true,
+            name: true,
+            email: true
+        }
+    });
+
+    if (!user) {
+        throw new HttpException('User not found', HttpStatus.NOT_FOUND);
+    }
+
+    return {
+        statusCode: HttpStatus.OK,
+        message: 'Profile',
+        data: user
+    }
+}
+
+  generateJWT(payload: any) {
+    return this.jwtService.sign(payload, {
+      secret: jwt_config.secret,
+      expiresIn: jwt_config.expired,
+    });
+  }
 }
